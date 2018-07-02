1b1924b5182a72902ccc5d06a69776a97ad70760
Ishaan Jain <ishaanj1@gmail.com>
2018-07-02 12:34:59 -0700

    added react-router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link } from "react-router-dom";
+
 import './App.css';
+import logo from './logo.svg';
 import TestComponent from './TestComponent.jsx';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
+        <nav>
+          <Link to="/" style={{marginRight: "20px"}}>Home</Link>
+          <Link to="/testComponent">Test Component</Link>
+        </nav>
+
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -14,7 +22,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.jsx</code> and save to reload.
         </p>
-        <TestComponent />
+
+        <hr />
+
+        <Route path="/testComponent" component={TestComponent} />
       </div>
     );
   }
