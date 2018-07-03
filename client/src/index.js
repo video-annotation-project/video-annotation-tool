9a18d0a0da206c12ad6980d04d0911e3a5e3340f
Ishaan Jain <ishaanj1@gmail.com>
2018-07-03 15:31:19 -0700

    removed service worker and fixed other bugs with production build

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
   <BrowserRouter>
     <App />
   </BrowserRouter>
 ), document.getElementById('root'));
-
-registerServiceWorker();
--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 const express = require('express');
-const fs = require('fs');
-// const sqlite = require('sql.js');
-
-// const filebuffer = fs.readFileSync('db/usda-nnd.sqlite3');
-
-// const db = new sqlite.Database(filebuffer);
-
+const path = require('path');
 const app = express();
 
 app.set('port', (process.env.PORT || 3001));
 
-// Express only serves static assets in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
-
 app.get('/api', (req, res) => {
   res.json('Test API reponse sent from Node js server');
 });
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.get('/*', (req, res) =>  {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
