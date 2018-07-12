fdc321633801f82a37da851f16d754dce95a88a0
Ishaan Jain <ishaanj1@gmail.com>
2018-07-12 11:59:17 -0700

    got some basic passport-jwt auth set up

--- server.js
+++ server.js
@@ -1,8 +1,82 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
+
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const passportJWT = require('passport-jwt');
+const ExtractJwt = passportJWT.ExtractJwt;
+const JwtStrategy = passportJWT.Strategy;
+
+var fakeUsers = [
+  {
+    id: 1,
+    name: 'test',
+    password: '123'
+  },
+  {
+    id: 2,
+    name: 'boi',
+    password: 'lol'
+  }
+];
+
+var jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = 'Bernie wouldve won';
+
+var strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
+  console.log('payload received', jwt_payload);
+  // usually this would be a database call:
+  var user = fakeUsers[_.findIndex(fakeUsers, {id: jwt_payload.id})];
+  if (user) {
+    next(null, user);
+  } else {
+    next(null, false);
+  }
+});
+
+passport.use(strategy);
+
 const app = express();
 
-app.set('port', (process.env.PORT || 3001));
+app.use(passport.initialize());
+// parse application/x-www-form-urlencoded
+// for easier testing with Postman or plain HTML forms
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+// parse application/json
+app.use(bodyParser.json())
+
+app.post("/login", function(req, res) {
+  if(req.body.name && req.body.password){
+    var name = req.body.name;
+    var password = req.body.password;
+  }
+  // usually this would be a database call:
+  var user = fakeUsers[_.findIndex(fakeUsers, {name: name})];
+  if( ! user ){
+    res.status(401).json({message:"no such user found"});
+  }
+
+  else if(user.password === req.body.password) {
+    // from now on we'll identify the user by the id and the id is the only
+    // personalized value that goes into our token
+    var payload = {id: user.id};
+    var token = jwt.sign(payload, jwtOptions.secretOrKey);
+    res.json({message: "ok", token: token});
+  } else {
+    res.status(401).json({message:"passwords did not match"});
+  }
+});
+
+app.get('/protected', passport.authenticate('jwt', {session: false}),
+  (req, res) => {
+    res.json("Success! You cannot see this without a token.");
+  }
+);
 
 app.get('/api', (req, res) => {
   res.json('Test API reponse sent from Node js server');
@@ -15,6 +89,8 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
+app.set('port', (process.env.PORT || 3001));
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
