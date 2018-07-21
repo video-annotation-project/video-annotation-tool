254d9295bbfccb3ea4fad6e2b3373c068cad485d
Ishaan Jain <ishaanj1@gmail.com>
2018-07-21 14:55:09 -0700

    worked on ConceptsList

--- client/src/components/Concepts.jsx
+++ client/src/components/Concepts.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
 import ConceptsList from './ConceptsList.jsx';
 
@@ -6,44 +7,13 @@ class Concepts extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      error: null,
-      isLoaded: false,
-      items: null
     };
   }
 
-  componentDidMount() {
-    fetch("/api/concepts", {
-      headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token')}
-    })
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            item: result
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-
   render() {
-    const { error, isLoaded, item } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    }
     return (
       <React.Fragment>
-        {isLoaded ? <div>{item}</div> : <div>Loading...</div>}
+        <Typography variant='display1' gutterBottom> Concepts </Typography>
         <ConceptsList />
       </React.Fragment>
     );
--- client/src/components/ConceptsList.jsx
+++ client/src/components/ConceptsList.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Collapse from '@material-ui/core/Collapse';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
 import StarBorder from '@material-ui/icons/StarBorder';
@@ -25,29 +23,85 @@ const styles = theme => ({
 });
 
 class ConceptsList extends React.Component {
-  state = { open: true };
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: true,
+      concepts: [
+        {
+          name: 'goldfish'
+        },
+        {
+          name: 'jellyfish'
+        },
+        {
+          name: 'sea turtle'
+        }
+      ],
+      isLoaded: false,
+      error: null,
+      item: null
+    };
+  }
+
+  componentDidMount() {
+    fetch("/api/concepts", {
+      headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token')}
+    })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            item: result
+          });
+        },
+        // Note: it's important to handle errors here instead of a catch() block
+        // so that we don't swallow exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error: error
+          });
+        }
+      );
+  }
 
   handleClick = () => {
     this.setState(state => ({ open: !state.open }));
   };
 
   render() {
+    const { error, isLoaded, item } = this.state;
     const { classes } = this.props;
 
+    if (error)  {
+      return <div>Error: {error.message}</div>;
+    }
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
     return (
       <div className={classes.root}>
-        <List
-          subheader={<ListSubheader>Nested List Items</ListSubheader>}
-        >
+        <div>{item}</div>
+        {this.state.concepts.map(concept => (
+          <ListItem>
+            <ListItemIcon>
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText inset primary={concept.name} />
+          </ListItem>
+        ))}
+        <List>
           <ListItem button onClick={this.handleClick}>
             <ListItemIcon>
-              <InboxIcon />
+              <StarBorder />
             </ListItemIcon>
             <ListItemText inset primary="Inbox" />
             {this.state.open ? <ExpandLess /> : <ExpandMore />}
           </ListItem>
           <Collapse in={this.state.open} timeout="auto" unmountOnExit>
-            <List component="div" disablePadding>
+            <List disablePadding>
               <ListItem button className={classes.nested}>
                 <ListItemIcon>
                   <StarBorder />
