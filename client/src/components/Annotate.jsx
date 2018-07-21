546b03c90aba45ea320518d3d6c8c8971cb785c7
hi4a4 <hanson4a4@gmail.com>
2018-07-21 13:26:47 -0700

    Working on style

--- client/src/components/Annotate.jsx
+++ client/src/components/Annotate.jsx
@@ -1,4 +1,86 @@
 import React, { Component } from 'react';
+import Rnd from 'react-rnd';
+import Button from '@material-ui/core/Button';
+import ReactDOM from 'react-dom';
+import { withStyles } from '@material-ui/core/styles';
+import CurrentConcepts from './CurrentConcepts.jsx';
+
+const styles = theme => ({
+  clear: {
+    clear: 'both'
+  },
+  videoSectionContainer: {
+    width: '1280px',
+    margin: '0 auto',
+    float: 'left'
+  },
+  videoContainer: {
+    float: 'left',
+    marginleft: '15px'
+  },
+  boxContainer: {
+    postion: 'relative',
+    top: '50px',
+    border: '1px black solid',
+    width: '1280px',
+    height: '723px'
+  },
+  playButton: {
+    margintop: '60px',
+    marginleft: '20px',
+    fontsize: '15px',
+    paddingtop: '10px',
+    paddingbottom: '10px'
+  }
+});
+
+function changeSpeed() {
+
+   try {
+   var myVideo = document.getElementById("video");
+   var speed = document.getElementById("playSpeed").value;
+   if ((speed / 100) === 0)
+   {
+      myVideo.playbackRate = (1);
+   }
+   else
+   {
+      myVideo.playbackRate = (speed / 100);
+   }
+   }
+   catch(err) {
+   alert("invalid input");
+   myVideo.playbackRate = 1;
+   }
+}
+
+function playPause() {
+   var myVideo = document.getElementById("video");
+   if(myVideo.paused)
+   {
+      myVideo.play();
+   }
+   else
+   {
+      myVideo.pause();
+   }
+
+}
+
+function fastForward() {
+   var myVideo = document.getElementById("video");
+   var cTime = myVideo.currentTime;
+   myVideo.currentTime = (cTime + 5);
+
+}
+
+function rewind() {
+   var myVideo = document.getElementById("video");
+   var cTime = myVideo.currentTime;
+   myVideo.currentTime = (cTime - 5);
+
+
+}
 
 class Annotate extends Component {
   constructor(props) {
@@ -10,42 +92,52 @@ class Annotate extends Component {
     };
   }
 
-  componentDidMount() {
-    fetch("/api/annotate", {
-      headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token')}
-    })
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            item: result
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-
   render() {
-    const { error, isLoaded, item } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <div>{item}</div>
-      );
-    }
+    const { classes } = this.props;
+
+    return (
+      <div>
+         <div className = {classes.clear}></div>
+         <div className = {classes.videoSectionContainer}>
+            <div className = {classes.videoContainer}>
+            <div className = {classes.boxContainer}>
+               <video id = "video" src = "fish2.mp4" width = "1280" height = "723" controls>
+               Your browser does not support the video tag.
+               </video>
+               <Rnd
+                 default = {{
+                    x: 30,
+                    y: 30,
+                    width: 60,
+                    height: 60,
+                 }}
+                 minWidth = {25}
+                 minHeight = {25}
+                 maxWidth = {400}
+                 maxHeight = {400}
+                 bounds = "parent"
+                 id = "dragBox"
+                 >
+               </Rnd>
+            </div>
+            </div>
+            <div className = "clear"></div>
+            <Button variant = "contained" color = "primary" id = "backwardButton1" onClick = {rewind}>-5 sec</Button>
+            <Button variant = "contained" color = "primary" className = {classes.playButton} onClick = {playPause}>Play/Pause</Button>
+            <Button variant = "contained" color = "primary" id = "forwardButton" onClick = {fastForward}>+5 sec</Button>
+            <br />
+            <span id = "playScript">Play at speed:</span>
+            <p><input type = "text" id = "playSpeed" placeholder = "100" />&ensp; %</p>
+            <input type = "submit" value = "Enter" id = "entered" onClick = {changeSpeed} />
+         </div>
+         <div id = "conceptSectionContainer">
+            <span id = "conceptsText">Current Concepts</span>
+            <br />
+            <CurrentConcepts />
+         </div>
+      </div>
+    );
   }
 }
 
-export default Annotate;
+export default withStyles(styles)(Annotate);
---
+++ client/src/components/CurrentConcepts.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class CurrentConcepts extends React.Component {
+   render() {
+      var concepts = ['concept1', 'concept2', 'concept3', 'concept4', 'concept5', 'concept6', 'concept7', 'concept8', 'concept9', 'concept10', 'concept11', 'concept12', 'concept13', 'concept14', 'concept15', 'concept16', 'concept17', 'concept18', 'concept19', 'concept20'];
+      var conceptsList = concepts.map(function(name){return <li className = "conceptListElement">{name} <br /> <img src = "fish1.png" alt = "Could not be downloaded" height="100" width="100" /></li>})
+      var leftList = [];
+      var rightList = [];
+      var conceptsListLength = conceptsList.length;
+      for (var i = 0; i < conceptsListLength; i++)
+      {
+         if ((i % 2) === 1)
+         {
+            rightList.push(conceptsList[i]);
+         }
+         else
+         {
+            leftList.push(conceptsList[i]);
+         }
+
+      }
+      return (
+                <div>
+                   <div id = "leftConcepts">
+                      <ul>{ leftList }</ul>
+                   </div>
+                   <div id = "rightConcepts">
+                      <ul>{ rightList }</ul>
+                   </div>
+                </div>
+             );
+   }
+
+}
+
+export default (CurrentConcepts);
