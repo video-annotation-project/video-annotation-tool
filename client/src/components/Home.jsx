b2062e56ee18658ad44289ab34a33bcf4fb043af
KevinThe2nd <kevinlabel@outlook.com>
2018-08-09 17:00:20 -0700

    updated homepage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import VideoList from './VideoList.jsx';
+import ErrorModal from './ErrorModal.jsx';
 
-const HomePage = () => (
-  <div>
-    <br />
-    hEY HEY HEY. welcome to the home page. make urself at home
-  </div>
-);
+const styles = theme => ({
+  clear: {
+    clear: 'both'
+  },
+  welcome: {
+    fontSize: '200%',
+    position: 'relative',
+    top: '15px',
+    left: '15px',
+    color: 'blue'
+  }
 
-export default HomePage;
+});
+
+function rewind() {
+   var myVideo = document.getElementById("video");
+   var cTime = myVideo.currentTime;
+   myVideo.currentTime = (cTime - 5);
+}
+
+
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      errorMsg: null,
+      open: false //For error modal box
+    };
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div>
+         <ErrorModal errorMsg={this.state.errorMsg} open={this.state.open} />
+         <div className = {classes.clear}></div>
+
+         <div className= {classes.welcome}>
+          Welcome Back!
+         </div>
+         <div className = {classes.clear}></div>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(HomePage);
