ad4f193f5eb0df0b10195147d263cd6cc00e22a8
jaketerrito <jaketerrito@github.com>
2019-02-08 16:51:39 -0800

    This is it?!

--- ml/load_n_train.py
+++ ml/load_n_train.py
@@ -16,6 +16,7 @@ import keras
 import pandas as pd
 from keras_retinanet.utils.model import freeze as freeze_model
 from keras.utils import multi_gpu_model
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument(
@@ -43,44 +44,43 @@ model_path = config['model_weights']
 bad_users = json.loads(os.getenv("BAD_USERS"))
 
 
-if True: 
-    folders = []
-    folders.append(img_folder)
-    for dir in folders:
-        if os.path.exists(dir):
-            shutil.rmtree(dir)
-        os.makedirs(dir)
+folders = []
+folders.append(img_folder)
+for dir in folders:
+    if os.path.exists(dir):
+        shutil.rmtree(dir)
+    os.makedirs(dir)
 
-    '''
-    Initializes the classmap of concept names to training id's.
-    (these id's don't represent the conceptid's from our database)
-    '''
-    start = time.time()
-    print("Initializing Classmap.")
+'''
+Initializes the classmap of concept names to training id's.
+(these id's don't represent the conceptid's from our database)
+'''
+start = time.time()
+print("Initializing Classmap.")
 
-    classmap = []
-    for concept in concepts:
-        name = queryDB("select name from concepts where id=" + str(concept)).iloc[0]["name"]
-        classmap.append([name,concepts.index(concept)])
-    classmap = pd.DataFrame(classmap)
-    classmap.to_csv(class_map_file,index=False, header=False)
-    classmap = classmap.to_dict()[0]
+classmap = []
+for concept in concepts:
+    name = queryDB("select name from concepts where id=" + str(concept)).iloc[0]["name"]
+    classmap.append([name,concepts.index(concept)])
+classmap = pd.DataFrame(classmap)
+classmap.to_csv(class_map_file,index=False, header=False)
+classmap = classmap.to_dict()[0]
 
-    end = time.time()
-    print("Done Initializing Classmap: " + str((end - start)/60) + " minutes")
+end = time.time()
+print("Done Initializing Classmap: " + str((end - start)/60) + " minutes")
 
 
-    '''
-    Downloads the annotation data and saves it into training and validation csv's.
-    Also downloads corresponding images.
-    '''
-    start = time.time()
-    print("Starting Download.")
+'''
+Downloads the annotation data and saves it into training and validation csv's.
+Also downloads corresponding images.
+'''
+start = time.time()
+print("Starting Download.")
 
-    download_annotations(min_examples, concepts, classmap, bad_users, img_folder, train_annot_file, valid_annot_file)
+download_annotations(min_examples, concepts, classmap, bad_users, img_folder, train_annot_file, valid_annot_file)
 
-    end = time.time()
-    print("Done Downloading Annotations: " + str((end - start)/60) + " minutes")
+end = time.time()
+print("Done Downloading Annotations: " + str((end - start)/60) + " minutes")
 
 
 '''
@@ -90,7 +90,7 @@ start = time.time()
 print("Starting Training.")
 
 model = models.backbone('resnet50').retinanet(num_classes=len(concepts), modifier=freeze_model)
-# model = multi_gpu_model(model, gpu=4)
+model = multi_gpu_model(model, gpu=4)
 model.load_weights(model_path, by_name=True, skip_mismatch=True)
 
 model.compile(
@@ -123,12 +123,16 @@ test_generator = CSVGenerator(
 )
 
 # Checkpoint: save models that are improvements
-filepath = "weights/weights-{epoch:02d}-{val_acc:.4f}.h5"
-checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=0, save_best_only=True, mode='max')
+filepath = "weights/weights-{epoch:02d}-{val_loss:.4f}.h5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_loss', save_best_only=True)
+
+#stopping: stops training if val_loss stops improving
+stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=5)
+
 
 history = model.fit_generator(train_generator, 
     epochs=10, 
-    callbacks=[checkpoint],
+    callbacks=[checkpoint, stopping],
     validation_data=test_generator
     ).history
 
