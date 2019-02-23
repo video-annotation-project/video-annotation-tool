e1a78e4f645eeaea9285f2d55af7b0457b3b7ab3
jaketerrito <jaketerrito@github.com>
2019-02-22 17:28:45 -0800

    Atleast it plays a couple frames

--- ml/predict.py
+++ ml/predict.py
@@ -21,7 +21,7 @@ s3 = boto3.client('s3', aws_access_key_id = AWS_ACCESS_KEY_ID, aws_secret_access
 S3_VIDEO_FOLDER = os.getenv('AWS_S3_BUCKET_VIDEOS_FOLDER')
 
 def main():
-   frames = get_video_frames("Ventana-3788_20140625T202207Z_01-59-42-20TC_h264.mp4")
+   frames = get_video_frames("DocRicketts-0569_20131213T224337Z_00-00-01-00TC_h264.mp4")
    print("initializing model")
    model = init_model()
    print("predicting")
@@ -81,11 +81,13 @@ def get_video_frames(video_name):
                       'Key': S3_VIDEO_FOLDER + video_name},
                        ExpiresIn = 100)
    vid = cv2.VideoCapture(url)
+   while not vid.isOpened():
+      continue
    print("getting video frames..")
    # put frames into frame list
    check = True
    counter = 0
-   while check and counter <= 500:  
+   while check and counter <= 20:  
       check, frame = vid.read()
       frames.append(frame)
       counter += 1
@@ -101,16 +103,16 @@ def predict_frames(video_frames, model):
       for tracked_object in currently_tracked_objects:
          tracked_object.set_unmatched()
       # try to match the currently tracked objects with the detections of this frame
-      detections = get_predictions(copy.deepcopy(frame), model)
-      for detection in detections:
+      detections = get_predictions(copy.deepcopy(video_frames[frame]), model)
+      #for detection in detections:
          # detection = class, bbox coords, confidence level
-         match, matched_object = does_match_existing_tracked_object(detection, currently_tracked_objects)
+         #match, matched_object = does_match_existing_tracked_object(detection, currently_tracked_objects)
          # if it matches, update the tracked object position
-         if match:
-            matched_object.update(detection)
+         #if match:
+         #   matched_object.update(detection)
          # else, assign unmatched detection to new tracked object
-         else:
-            currently_tracked_objects.add(tracked_object(detection))
+         #else:
+         #   currently_tracked_objects.add(tracked_object(detection))
       # clean up unmatched tracked objects
       if (frame_counter + NUM_FRAMES - 1 >= len(video_frames)):
          next_frames = video_frames[frame_counter : ]
@@ -119,14 +121,14 @@ def predict_frames(video_frames, model):
       tracked_frames = get_tracked_frames(detections, next_frames)
       frame_list.append(tracked_frames)
       frame_counter += NUM_FRAMES
-      for tracked_object in currently_tracked_objects:
-         if not tracked_object.matched:
-            currently_tracked_objects.remove(tracked_object)
+      #for tracked_object in currently_tracked_objects:
+      #   if not tracked_object.matched:
+      #      currently_tracked_objects.remove(tracked_object)
    return frame_list
  
-def get_predictinos(frame, model):
+def get_predictions(frame, model):
    frame = np.expand_dims(frame, axis=0)
-   boxes, scores, labels = model.predict_on_batch(inputs)
+   boxes, scores, labels = model.predict_on_batch(frame)
 
    thresholds = [.1,.1,.1,.1,.1]
    predictions = zip (boxes[0],scores[0],labels[0])
@@ -135,13 +137,14 @@ def get_predictinos(frame, model):
       if thresholds[label] > score:
          continue
       filtered_predictions.append((box,score,label))
+   return filtered_predictions
 
 def get_tracked_frames(detections, video_frames):
    frame_list = [] 
    trackers = cv2.MultiTracker_create()
    frame = video_frames[0]
    for detection in detections:
-      (x1, y1, x2, y2) = detections[0]
+      (x1, y1, x2, y2) = detection[0]
       width = x2 - x1
       height = y2 - y1
       box = (x1, y1, width, height)
@@ -153,13 +156,11 @@ def get_tracked_frames(detections, video_frames):
             (x, y, w, h) = [int(v) for v in box]
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
          frame_list.append(frame)
-         cv2.imshow("Frame", frame)
-         cv2.waitKey(1)
    return frame_list
 
 
 def does_match_existing_tracked_object(detection, currently_tracked_objects):
-   return True
+   return True, "Nothing"
    #UPDATE - determine confidence level + distance metric to deem match or not
 
 
