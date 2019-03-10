b9ecf597b7eb9ba48af0c2f84c4573c668544511
Ubuntu <ubuntu@ip-172-31-29-229.us-west-1.compute.internal>
2019-03-10 03:14:55 +0000

    needs fixing

--- ml/predict.py
+++ ml/predict.py
@@ -35,9 +35,33 @@ with open(config_path) as config_buffer:
 
 model_path = config['model_weights']
 
+trackers = cv2.MultiTracker_create()
+
+class Tracked_object:
+
+   def __init__(self, detection):
+      (x1, y1, x2, y2) = detection[0]
+      self.x1 = x1
+      self.x2 = x2
+      self.y1 = y1
+      self.y2 = y2
+#      self.concept = concept
+      self.matched = True
+
+   def update(self, detection):
+      (x1, y1, x2, y2) = detection[0]
+      self.x1 = x1
+      self.x2 = x2
+      self.y1 = y1
+      self.y2 = y2
+      self.matched = True
+
+   def set_unmatched(self):
+      self.matched = False
+
 def main():
    print("Loading Video")
-   frames = get_video_frames("DocRicketts-0569_20131213T224337Z_00-00-01-00TC_h264.mp4")
+   frames = get_video_frames("DocRicketts-0701_20141216T212020Z_00-48-12-26TC_h264.mp4")
    print("Initializing Model")
    model = init_model()
    print("Predicting")
@@ -52,13 +76,14 @@ def get_video_frames(video_name):
                       'Key': S3_VIDEO_FOLDER + video_name},
                        ExpiresIn = 100)
    vid = cv2.VideoCapture(url)
+   print(url)
    while not vid.isOpened():
       continue
    print("Successfully opened video.")
    # put frames into frame list
    check = True
-   while True:
-   #for i in range(0,100): 
+#   while True:
+   for i in range(0, 3750): 
       check, frame = vid.read()
       if not check:
          break
@@ -78,28 +103,26 @@ def predict_frames(video_frames, model):
    frame_list = []
    for frame in range(0, len(video_frames), NUM_FRAMES):
       # set each tracked object as unmatched for new frame
-      #for tracked_object in currently_tracked_objects:
-      #   tracked_object.set_unmatched()
-      # try to match the currently tracked objects with the detections of this frame
+      for tracked_object in currently_tracked_objects:
+         tracked_object.set_unmatched()
       detections = get_predictions(copy.deepcopy(video_frames[frame]), model)
-      #for detection in detections:
+      # try to match the currently tracked objects with the detections of this frame
+      for detection in detections:
          # detection = class, bbox coords, confidence level
-         #match, matched_object = does_match_existing_tracked_object(detection, currently_tracked_objects)
-         # if it matches, update the tracked object position
-         #if match:
-         #   matched_object.update(detection)
-         # else, assign unmatched detection to new tracked object
-         #else:
-         #   currently_tracked_objects.add(tracked_object(detection))
-      # clean up unmatched tracked objects
+         match, matched_object = does_match_existing_tracked_object(detection, currently_tracked_objects)
+         if match:
+             matched_object.update(detection)
+         else:
+            currently_tracked_objects.append(Tracked_object(detection))
       if (frame + NUM_FRAMES - 1 >= len(video_frames)):
          stop = len(video_frames)
       else:
          stop = frame + NUM_FRAMES - 1
-      track_detections(detections, video_frames,frame,stop)
-      #for tracked_object in currently_tracked_objects:
-      #   if not tracked_object.matched:
-      #      currently_tracked_objects.remove(tracked_object)
+      # clean up unmatched tracked objects
+      for tracked_object in currently_tracked_objects:
+         if not tracked_object.matched:
+            currently_tracked_objects.remove(tracked_object)
+      track_detections(currently_tracked_objects, video_frames, frame, stop)
    return video_frames
 
 def get_predictions(frame, model):
@@ -117,16 +140,17 @@ def get_predictions(frame, model):
 
 # Tracks and adds bounding boxes for detected objects in video frames from start to stop frame.
 # Works in place, so no extra memory use! Woot
-def track_detections(detections, video_frames, start ,stop):
+# add new tracker for unmatched objects, use same tracker for matched objects
+def track_detections(currently_tracked_objects, video_frames, start, stop):
+   global trackers
    frame_list = [] 
-   trackers = cv2.MultiTracker_create()
    frame = video_frames[start]
-   for detection in detections:
-      (x1, y1, x2, y2) = detection[0]
-      width = x2 - x1
-      height = y2 - y1
-      box = (x1, y1, width, height)
-      trackers.add(cv2.TrackerKCF_create(), frame, box)
+   for obj in currently_tracked_objects:
+      if not obj.matched:
+         width = obj.x2 - obj.x1
+         height = obj.y2 - obj.y1
+         box = (obj.x1, obj.y1, width, height)
+         trackers.add(cv2.TrackerKCF_create(), frame, box)
    for frame in video_frames[start:stop]:
       (success, boxes) = trackers.update(frame)
       if success:
@@ -138,35 +162,40 @@ def display_video(frames):
    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
    out = cv2.VideoWriter('output.mp4',fourcc, 30.0, frames[0].shape[::-1][1:3])
    for frame in frames:
-      cv2.imshow("Frame", frame)
-      cv2.waitKey(int(1000/30))
+#      cv2.imshow("Frame", frame)
+#      cv2.waitKey(int(1000/30))
       out.write(frame)
    out.release()
    cv2.destroyAllWindows()
 
 def does_match_existing_tracked_object(detection, currently_tracked_objects):
-   return True, "Nothing"
-   #UPDATE - determine confidence level + distance metric to deem match or not
-
-class tracked_object:
-
-   def __init__(self, x1, x2, y1, y2, concept):
-      self.x1 = x1
-      self.x2 = x2
-      self.y1 = y1
-      self.y2 = y2
-      self.concept = concept
-      self.matched = True
-
-   def update(self, x1, x2, y1, y2):
-      self.x1 = x1
-      self.x2 = x2
-      self.y1 = y1
-      self.y2 = y2
-      self.matched = True
-
-   def set_unmatched(self):
-      self.matched = False
+    # calculate IOU for each
+   max_iou = 0 
+   match = None
+   for obj in currently_tracked_objects:
+       (x1, y1, x2, y2) = detection[0]
+       # determine the (x, y)-coordinates of the intersection rectangle
+       xA = max(x1, obj.x1)
+       yA = max(y1, obj.y1)
+       xB = min(x2, obj.x2)
+       yB = min(y2, obj.y2)
+       # compute the area of intersection rectangle
+       interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+       # compute the area of both the prediction and ground-truth
+       # rectangles
+       boxAArea = (x2 - x1 + 1) * (y2 - y1 + 1)
+       boxBArea = (obj.x2 - obj.x1 + 1) * (obj.y2 - obj.y1 + 1)
+       # compute the intersection over union by taking the intersection
+       # area and dividing it by the sum of prediction + ground-truth
+       # areas - the interesection area
+       iou = interArea / float(boxAArea + boxBArea - interArea)
+       if (iou > max_iou):
+          max_iou = iou
+          match = obj
+   if max_iou >= 0.70:               
+      print("match")
+      return True, match
+   return False, None
 
 if __name__ == '__main__':
   main()
