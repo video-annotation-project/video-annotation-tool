544060998cd9e61dc7d2d855ddba0a609cdf68dd
samgzl <sgunzl@calpoly.edu>
2019-05-02 16:23:15 -0700

    working on getting speed of all annotations

---
+++ ml/speed.py
@@ -0,0 +1,67 @@
+# Initial package imports
+#from pgdb import connect
+import json
+import math
+import pandas as pd
+import psycopg2
+from PIL import Image
+import boto3
+import numpy as np
+import os
+from dotenv import load_dotenv
+from pascal_voc_writer import Writer
+import random
+import math
+
+load_dotenv(dotenv_path="../.env")
+S3_BUCKET = os.getenv('AWS_S3_BUCKET_NAME')
+SRC_IMG_FOLDER = os.getenv('AWS_S3_BUCKET_ANNOTATIONS_FOLDER')
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+FPS = 29.97002997002997
+
+
+client = boto3.client('s3',
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+
+def main():
+   get_annotation_speed([7,8,9,10,14,4,5,19])
+
+def get_annotation_speed(bad_users):
+    conn = psycopg2.connect(database = DB_NAME,
+                        user = DB_USER,
+                        password = DB_PASSWORD,
+                        host = DB_HOST,
+                        port = "5432")
+    cur = conn.cursor()
+    bad_users.append(17) # dont get opencv tracking annotations
+    # get human annotations that haven't had speed calculated yet
+    query = "SELECT * FROM annotations WHERE speed IS NULL AND userid NOT IN " + str(tuple(bad_users)) + " LIMIT 500"
+    df = pd.read_sql_query(query, conn)
+    for index, row in df.iterrows():
+#       print(row.id, row.timeinvideo)
+        # get tracking annotations within 1 sec after original annot
+       query = "SELECT * FROM annotations WHERE userid=17 AND originalid=%d AND timeinvideo BETWEEN %f AND %f LIMIT 1" % (row.id, row.timeinvideo, row.timeinvideo + 1)
+       tracking_annot  = pd.read_sql_query(query, conn)
+#       print(tracking_annot.timeinvideo[0])
+       (x1, y1) = get_center(row.x1, row.x2, row.y1, row.y2)
+       (x2, y2) = get_center(tracking_annot.x1[0], tracking_annot.x2[0], tracking_annot.y1[0], tracking_annot.y2[0])
+       # resize tracking bounding box to match original 
+       # annotation dimensions if video is not 1280x720
+       # (all tracking annotations are stored as 1280x720 frames)
+       if (row.videowidth != 1280 or row.videoheight != 720): 
+          x2 = (x2 / 1280) * row.videowidth
+          y2 = (y2 / 720) * row.videoheight
+       dist = math.sqrt( (x2 - x1)**2 + (y2 - y1)**2 ) 
+       print(dist, row.id)
+
+def get_center(x1, x2, y1, y2):
+   return (((x2 - x1) / 2) + x1, ((y2 - y1) / 2) + y1)
+
+
+if __name__ == '__main__':
+   main()
