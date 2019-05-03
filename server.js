8bda001d0b3a6bc5ef4f324181b63b240d21085b
Ishaan Jain <ishaanj1@gmail.com>
2019-05-03 12:41:20 -0700

    turned /api/cookies route into a helpful function that's invoked from /api/login, and debugged

--- server.js
+++ server.js
@@ -54,6 +54,51 @@ app.use(passport.initialize());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
+// This function sets the cookies that are used by the client to access the 
+// videos on AWS CloudFront
+const makeCookies = (req, res) => {
+  console.log('Making cookie...');
+  const keyPairId = process.env.KEY_PAIR_ID;
+  const privateKey = process.env.RSA_PRIVATE_KEY.split('\\n').join('\n');
+  let cfUrl = "cdn.deepseaannotations.com";
+  let expiry = Math.floor(Date.now() / 1000) + 60000;
+
+  let signer = new AWS.CloudFront.Signer(keyPairId, privateKey);
+  let policy = {
+    'Statement': [{
+      'Resource': 'http*://' + cfUrl + '/*',
+      'Condition': {
+        'DateLessThan': { 'AWS:EpochTime': expiry }
+      }
+    }]
+  };
+  let policyString = JSON.stringify(policy);
+  var options = { url: "http://" + cfUrl, policy: policyString };
+  signer.getSignedCookie(options, (error, cookie) => {
+    if (error) {
+      console.log('Error recieved from getSignedCookie function.')
+      console.log('Throwing error.')
+      throw error;
+    }
+    res.cookie('CloudFront-Key-Pair-Id', cookie['CloudFront-Key-Pair-Id'], {
+      domain: '.deepseaannotations.com',
+      path: '/',
+      // httpOnly: true
+    });
+    res.cookie('CloudFront-Policy', cookie['CloudFront-Policy'], {
+      domain: '.deepseaannotations.com',
+      path: '/',
+      // httpOnly: true
+    });
+    res.cookie('CloudFront-Signature', cookie['CloudFront-Signature'], {
+      domain: '.deepseaannotations.com',
+      path: '/',
+      // httpOnly: true
+    });
+    console.log(cookie);
+  });
+}
+
 app.post("/api/login", async function (req, res) {
   const { username, password } = req.body;
   let queryPass = 'select id, password, admin \
@@ -70,6 +115,7 @@ app.post("/api/login", async function (req, res) {
     }
     const payload = { id: user.rows[0].id };
     const token = jwt.sign(payload, jwtOptions.secretOrKey);
+    makeCookies(req, res);
     res.json({
       token: token,
       isAdmin: user.rows[0].admin
@@ -730,59 +776,6 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('refresh videos');
   });
 });
-
-// This route serves the cookies that are used by the client to access the 
-// videos on AWS CloudFront
-app.get('/api/cookies', passport.authenticate('jwt', { session: false }),
-  async (req, res) => {
-    try {
-      console.log('Making cookie...');
-      const privateKey = process.env.RSA_PRIVATE_KEY;
-      const keyPairId = process.env.KEY_PAIR_ID;
-      let cfUrl = "cdn.deepseaannotations.com";
-      let expiry = Math.floor(Date.now() / 1000) + 60000;
-
-      let signer = new AWS.CloudFront.Signer(keyPairId, privateKey);
-      let policy = {
-        'Statement': [{
-          'Resource': 'http*://' + cfUrl + '/*',
-          'Condition': {
-            'DateLessThan': { 'AWS:EpochTime': expiry }
-          }
-        }]
-      };
-      let policyString = JSON.stringify(policy);
-      var options = { url: "http://" + cfUrl, policy: policyString };
-      signer.getSignedCookie(options, (error, cookie) => {
-        if (error) {
-          console.log('Error recieved from getSignedCookie function.')
-          console.log('Throwing error.')
-          throw error;
-        }
-        res.cookie('CloudFront-Key-Pair-Id', cookie['CloudFront-Key-Pair-Id'], {
-          domain: '.cdn.deepseaannotations.com',
-          path: '/',
-          // httpOnly: true
-        });
-        res.cookie('CloudFront-Policy', cookie['CloudFront-Policy'], {
-          domain: '.cdn.deepseaannotations.com',
-          path: '/',
-          // httpOnly: true
-        });
-        res.cookie('CloudFront-Signature', cookie['CloudFront-Signature'], {
-          domain: '.cdn.deepseaannotations.com',
-          path: '/',
-          // httpOnly: true
-        });
-        res.send(cookie);
-        console.log(cookie);
-      });
-    } catch(error) {
-      console.log(error);
-      res.status(500).json(error);
-    }
-  }
-);
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
