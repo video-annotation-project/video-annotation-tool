579cbe6d1adaa4b0d67139031a12bb57ec9c1726
hi4a4 <hanson4a4@gmail.com>
2019-06-24 15:49:17 -0700

    In ml/loading_data.py: Changed format of code, added csv output of images that failed to load (for future image generation, and ignore image download for images that already exist

--- ml/loading_data.py
+++ ml/loading_data.py
@@ -10,6 +10,7 @@ from dotenv import load_dotenv
 from pascal_voc_writer import Writer
 import random
 import speed
+import time
 
 load_dotenv(dotenv_path="../.env")
 S3_BUCKET = os.getenv('AWS_S3_BUCKET_NAME')
@@ -57,43 +58,37 @@ def select_annotations(annotations, min_examples, selected_concepts):
     for concept in selected_concepts:
         concept_count[concept] = 0
 
-    # Get's fps for each video in order to calculate frame_num from timeinvideo
-    videos = pd.DataFrame(annotations['videoid'].unique(),columns = ['videoid'])
-    videos['fps'] = videos['videoid'].apply(lambda x: queryDB('select * from videos where id = ' +str(x)).iloc[0].fps)
-    annotations = annotations.merge(videos, left_on='videoid', right_on='videoid')
+    group_frame = annotations.groupby(['videoid','frame_num'], sort=False)
+    group_frame = [df for _, df in group_frame]
+    random.shuffle(group_frame) # Shuffle BEFORE the sort
 
-    annotations['frame_num'] = np.rint(annotations['timeinvideo'] * annotations['fps'])
-
-    groups = annotations.groupby(['videoid','frame_num'], sort=False)
-    groups = [df for _, df in groups]
-    random.shuffle(groups) # Shuffle BEFORE the sort
-
-    # sorts into two 'groups' - those without ai annotations from low to high speed (0, speed) 
-    # (note: the speed is irrelevant here since they are the original human annotations) and 
-    # those with ai annotations from low to high speed (1, speed) - prioritizing frames with no tracking
-    # annotations and then those with tracking but low average speeds (more accurate tracking?)
     ai_id = queryDB("SELECT id FROM users WHERE username='tracking'").id[0]
-    sort_lambda = lambda df : (set(df['userid']) == set([ai_id]), df.speed.mean())
-    groups.sort(key=sort_lambda)
+    # Give priority to frames with least amount of tracking annotations
+    # And lower speed
+    sort_lambda = lambda df : (df['userid'].count(ai_id), df.speed.mean())
+    group_frame.sort(key=sort_lambda)
 
     #selects images that we'll use (each group has annotations for an image)
-    for group in groups:
+    for frame in group_frame:
+        # Check if we have min number of images already
         if not any(v < min_examples for v in concept_count.values()):
             break
+        
         in_annot = []
-        for index, row in group.iterrows():
+        for index, row in frame.iterrows():
             concept_count[row['conceptid']] += 1
             in_annot.append(row['conceptid'])
-        #checks if we have more of one concept than we want
+
+        # Checks if frame has only concept we have too many of
         if any(v > min_examples for v in concept_count.values()):
-            #gets all concepts that we have to many of
+            #gets all concepts that we have too many of
             excess = list({key:value for (key,value) in concept_count.items() if value > min_examples})
             #don't save the annotation if it doens't include concept that we need more of
             if set(excess) >= set(in_annot):
                 for a in in_annot:
                     concept_count[a] -= 1
                 continue
-        selected.append(group)
+        selected.append(frame)
     return selected, concept_count
 
 
@@ -108,23 +103,41 @@ def select_annotations(annotations, min_examples, selected_concepts):
 #   valid_annot_file: name of validation annotations csv
 #   split: fraction of annotation images that willbe used for training (rest used in validation)
 def download_annotations(min_examples, concepts, selected_concepts, concept_map, users, videos, img_folder, train_annot_file, valid_annot_file, split=.8):
+    # Variable that represents all images already in the image folder
+    existing_images = set(os.listdir(img_folder))
+    
     # Get all annotations for given concepts (and child concepts) making sure that any tracking annotations originated from good users
     users = "\'" +  ','.join(str(e) for e in users) + "\'"
     videos = "\'" + ','.join(str(e) for e in videos) + "\'"
     str_concepts = "\'" + ','.join(str(e) for e in selected_concepts) + "\'"
 
+    # To-do: change to prepared statement
     annotations = queryDB(
-        ''' SELECT *
+        f'''SELECT 
+              id,
+              image,
+              videoid,
+              videowidth,
+              videoheight,
+              conceptid,
+              x1, x2, y1, y2,
+              fps*timeinvideo as frame_num
             FROM annotations as A
-            WHERE conceptid::text = ANY(string_to_array(''' + str_concepts + ''',','))
-            AND videoid::text = ANY(string_to_array(''' + videos + ''',','))
+            WHERE conceptid::text = ANY(string_to_array({str_concepts},','))
+            AND videoid::text = ANY(string_to_array({videos},','))
             AND EXISTS ( 
                 SELECT id, userid 
                 FROM annotations 
                 WHERE id=A.originalid 
                 AND unsure = False
-                AND userid::text = ANY(string_to_array(''' + users + ",',')))")
+                AND userid::text = ANY(string_to_array({users},',')))
+            LEFT JOIN videos ON videos.id=videoid''')
 
+    '''
+    Returns
+    selected - list of frames (frame is a group of annotations at same frame)
+    concept_count - dictionary of conceptid and number of annotations within selected
+    '''
     selected, concept_count = select_annotations(annotations, min_examples, selected_concepts)
     print("Concept counts: " + str(concept_count))
     print("Number of images: " + str(len(selected)))
@@ -132,22 +145,27 @@ def download_annotations(min_examples, concepts, selected_concepts, concept_map,
     df_train_annot = pd.DataFrame()
     df_valid_annot = pd.DataFrame()
     count = 0
-    for group in selected:
-        first = group.iloc[0]
+    failed_to_load_images = []
+    for frame in selected:
+        # Get image for first annotation in frame
+        first = frame.iloc[0]
         img_location = img_folder + "/" + str(first['image'])
         if ".png" not in img_location:
            img_location += ".png"
-        
-        try:
-            # try to download image. 
-            obj = client.get_object(Bucket=S3_BUCKET, Key= SRC_IMG_FOLDER + str(first['image']))
-            img = Image.open(obj['Body'])
-            img.save(img_location)
-        except:
-            print("Failed to load image:" + str(first['image']))
-            continue
 
-        for index, row in group.iterrows():
+        # Check if image already exists in image folder
+        if str(first['image']) not in existing_images:
+            try:
+                # try to download image. 
+                obj = client.get_object(Bucket=S3_BUCKET, Key= SRC_IMG_FOLDER + str(first['image']))
+                img = Image.open(obj['Body'])
+                img.save(img_location)
+            except:
+                print("Failed to load image:" + str(first['image']))
+                failed_to_load_images.append(str(first['id']))
+                continue
+
+        for index, row in frame.iterrows():
             concept_index = concepts.index(row['conceptid'])
             if (int(row['videowidth']) != int(first['videowidth'])):
                x_ratio = (row['videowidth'] / first['videowidth'])
@@ -182,6 +200,7 @@ def download_annotations(min_examples, concepts, selected_concepts, concept_map,
                     concept_map[concept_index]]])
         count += 1
 
+    pd.DataFrame({'Missing_Image_Annotation_ID': failed_to_load_images}).to_csv(time.ctime()+'missing_images.csv')
     df_train_annot.to_csv(path_or_buf=train_annot_file, header=False, index=False)
     df_valid_annot.to_csv(path_or_buf=valid_annot_file, header=False, index=False)
 
