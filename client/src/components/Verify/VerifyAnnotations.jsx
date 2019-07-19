1d8ac815f7c88d97056bca803d9f0b255dd4909a
Kyaw Soe <32246763+kls551@users.noreply.github.com>
2019-07-18 20:11:38 -0700

    Update VerifyAnnotations.jsx

--- client/src/components/Verify/VerifyAnnotations.jsx
+++ client/src/components/Verify/VerifyAnnotations.jsx
@@ -26,9 +26,7 @@ const styles = theme => ({
     margin: theme.spacing()
   },
   img: {
-    top: "50px",
-    width: "1600px",
-    height: "900px"
+    top: "50px"
   },
   container: {
     display: "grid",
@@ -525,7 +523,12 @@ class VerifyAnnotations extends Component {
                   keyName="r, d, i, v"
                   onKeyDown={this.handleKeyDown.bind(this)}
                 / >
-                <div>
+                <div
+                  style={{
+                    width: annotation.videowidth,
+                    height: annotation.videoheight
+                  }}
+                >
                   <DragBoxContainer
                     className={classes.img}
                     dragBox={classes.dragBox}
