f9fd91cced27ebc3b466ee1e71e2fcc038a5b342
hi4a4 <hanson4a4@gmail.com>
2019-08-05 15:34:35 -0700

    In trackingAnnotations/fix_offset.py: made more efficient and larger search range

--- trackingAnnotations/fix_offset.py
+++ trackingAnnotations/fix_offset.py
@@ -33,8 +33,24 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 # video/image properties
-VIDEO_WIDTH = 1600
-VIDEO_HEIGHT = 900
+VIDEO_WIDTH = 1920
+VIDEO_HEIGHT = 1080
+
+# Generator to iterate through video frames
+def iter_from_middle(lst):
+  try:
+      middle = math.ceil(len(lst)/2)
+      yield middle, lst[middle]
+
+      for shift in range(1, middle + 1):
+          left = middle - shift
+          right = middle + shift
+          yield left, lst[left]
+          yield right, lst[right]
+
+  except IndexError: # occures on lst[len(lst)] or for empty list
+      # raise StopIteration
+      return
 
 def fix_offset(annotation):
    con = connect(database=DB_NAME, host=DB_HOST, user=DB_USER, password=DB_PASSWORD)
@@ -53,20 +69,19 @@ def fix_offset(annotation):
    fps = cap.get(cv2.CAP_PROP_FPS)
 
    #search within +- search range seconds of original, +- frames from original annotation
-   frames = 20
-   search_range = 1/fps * frames
+   frames = 40
+   search_seconds_range = frames / fps
 
    # initialize video for grabbing frames before annotation
-   cap.set(0, (annotation.timeinvideo-search_range)*1000) # tell video to start at 'start'-1 time
+   cap.set(0, (annotation.timeinvideo-search_seconds_range)*1000) # tell video to start at 'start'-1 time
    
    imgs = []
    times = []
-   for i in range(math.ceil(fps*search_range * 2)):
-    check, vid = cap.read()
+   for i in range(math.ceil(fps*search_seconds_range * 2)):
+    check, img = cap.read()
     if not check:
       print("end of video reached")
       break
-    img = imutils.resize(vid, width=VIDEO_WIDTH, height=VIDEO_HEIGHT)
     time = cap.get(cv2.CAP_PROP_POS_MSEC)
     times.append(time)
     imgs.append(img)
@@ -89,24 +104,36 @@ def fix_offset(annotation):
    img = img[:,:,::-1]
    img = cv2.resize(img, (VIDEO_WIDTH, VIDEO_HEIGHT))
 
-   best_score = 0
-   best = None  
-   for i in range(math.ceil(fps*search_range)):
-    # +1 or -1
-    for s in range(-1,2,2):
-      index = math.ceil(fps*search_range) + i * s
+   for index, img in iter_from_middle(imgs):
       if index == len(imgs):
           continue
-      (score, diff) = compare_ssim(img, imgs[index], full=True, multichannel=True)
-      if best_score < score:
-        best = index
-        best_score = score
+      (score, _) = compare_ssim(img, imgs[index], full=True, multichannel=True)
 
-      if best_score > .95:
-        cursor.execute("UPDATE annotations SET timeinvideo=%f, originalid=NULL WHERE id=%d;",(times[best]/1000, annotation.id,))
+      if score > .95:
+        cursor.execute(
+          '''
+          UPDATE annotations
+          SET framenum=%d, timeinvideo=%f, originalid=NULL 
+          WHERE id= %d;''',
+          (round(times[index]*fps/1000),times[index]/1000, annotation.id,))
         con.commit()
         con.close()
         return
    cursor.execute("UPDATE annotations SET unsure=TRUE WHERE id=%d;",(annotation.id,))
    con.commit()
    con.close()
+
+if __name__ == "__main__":
+   con = connect(database=DB_NAME, host=DB_HOST, user=DB_USER, password=DB_PASSWORD)
+   cursor = con.cursor()
+   cursor.execute(
+   '''
+   SELECT id, timeinvideo, videoid
+   FROM annotations
+   WHERE id IN (4973786, 4984018, 4984048, 4984743, 4985958, 4987375, 4987406, 4989155, 4989837, 4990318, 4990521, 4990596, 4990860, 4991687, 4991913, 4992313, 4992324, 4992507, 4996036, 4998312, 5000857, 5002358, 5005777)
+   '''
+   )
+
+   rows = cursor.fetchall()
+   for row in rows:
+     fix_offset(row)
