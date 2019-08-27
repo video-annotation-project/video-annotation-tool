9fa7e31bfa964a54474fa2f263eea5ba5d4207ad
jccho16 <jccho@calpoly.edu>
2019-08-26 23:47:12 -0700

    More minor changes

--- client/src/components/AIVideos/AIvideos.jsx
+++ client/src/components/AIVideos/AIvideos.jsx
@@ -11,7 +11,7 @@ import Grid from '@material-ui/core/Grid';
 import DragBoxContainer from '../Utilities/DragBoxContainer';
 import AIvideoList from './AIvideoList';
 
-const styles = () => ({
+const styles = theme => ({
   videoContainer: {
     top: '60px',
     width: '1600px',
@@ -21,6 +21,9 @@ const styles = () => ({
     marginTop: '10px',
     marginLeft: '20px',
     marginBottom: '10px'
+  },
+  videoName: {
+    marginTop: theme.spacing(1.5)
   }
 });
 
@@ -221,7 +224,11 @@ class Annotate extends Component {
             />
           </Grid>
           <Grid item xs>
-            <Typography variant="h5">
+            <Typography
+              variant="h5"
+              align="center"
+              className={classes.videoName}
+            >
               {`${currentVideo.id} ${currentVideo.filename}`}
             </Typography>
           </Grid>
