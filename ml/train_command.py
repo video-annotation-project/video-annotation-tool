06bd2bb29bb0ddc241c2e7f4deb2a8af43858652
samgzl <sgunzl@calpoly.edu>
2019-10-06 18:04:47 -0700

    debug

--- ml/train_command.py
+++ ml/train_command.py
@@ -56,7 +56,7 @@ verifyVideos = model["verificationvideos"]
 # from model_version, select versions one level down
 level_down = pd_query(
     """ SELECT version FROM model_versions WHERE model=%s AND version ~ '%s.*{1}' """,
-    (str(model_params["model"]),
+    (model_params["model"],
     model_version)
 )
 
