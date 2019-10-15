449735f2a6f6a9992d74a817124391d22f7cb043
hi4a4 <hanson4a4@gmail.com>
2019-10-15 02:58:25 -0700

    ,

--- ml/train/preprocessing/annotation_generator.py
+++ ml/train/preprocessing/annotation_generator.py
@@ -239,7 +239,7 @@ class AnnotationGenerator(object):
                 conceptid,
                 x1, x2, y1, y2,
                 speed,
-                ROUND(fps * timeinvideo) as frame_num
+                ROUND(fps * timeinvideo) as frame_num,
                 verifiedby
             FROM
                 annotation_intermediate inter
