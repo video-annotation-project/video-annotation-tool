53d2817df0412acbc7d36865ebdbb744e1df949b
justin <jccho@calpoly.edu>
2020-02-07 01:52:46 -0800

    Models table now sortable

--- server/routes/models/index.js
+++ server/routes/models/index.js
@@ -66,7 +66,6 @@ router.get(
       ) av ON av.model_name = m.name
       GROUP BY
         (m.name, m.timestamp, verificationvideos, versions, start_trains, version_selected)
-      ORDER BY m.timestamp DESC
     `;
     if (req.query.predict === 'true') {
       queryText = `
