82fe3891bb0b23e09622140bd48130251d8bb0c4
Viet Nguyen <vnguy143@calpoly.edu>
2020-06-17 20:22:36 -0700

    Fix query that selects annotations to remove ones from verify videos

--- ml/train/preprocessing/annotation_generator.py
+++ ml/train/preprocessing/annotation_generator.py
@@ -265,7 +265,7 @@ class AnnotationGenerator(object):
                 annotations a ON a.id=inter.annotationid
             LEFT JOIN
                 videos ON videos.id=videoid
-            WHERE inter.id = ANY(%s) AND a.videoid <> ANY(%s)
+            WHERE inter.id = ANY(%s) AND a.videoid <> ALL(%s)
         '''
 
         if verified_only:
