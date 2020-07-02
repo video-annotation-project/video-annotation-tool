252b5ab85740998e7927015256ef94a5925a8a35
Hanson Egbert <mainhanson@gmail.com>
2020-07-02 11:47:09 -0700

    In run_training.sh: changed ec2-user to ubuntu:

--- ml/run_training.sh
+++ ml/run_training.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd /home/ec2-user/video-annotation-tool/ml/
+cd /home/ubuntu/video-annotation-tool/ml/
 source activate tensorflow2_p36
-python3 -u /home/ec2-user/video-annotation-tool/ml/train_command.py > results.txt 2> error.txt &
+python3 -u /home/ubuntu/video-annotation-tool/ml/train_command.py > results.txt 2> error.txt &
