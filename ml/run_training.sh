439d316316837e978464fa65cd3257212dd65eee
Hanson Egbert <mainhanson@gmail.com>
2020-07-02 11:47:57 -0700

    In run_training.sh: changed python environments

--- ml/run_training.sh
+++ ml/run_training.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ubuntu/video-annotation-tool/ml/
-source activate tensorflow2_p36
+workon cv
 python3 -u /home/ubuntu/video-annotation-tool/ml/train_command.py > results.txt 2> error.txt &
